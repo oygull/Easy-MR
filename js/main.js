b883306a137d460fb9c8dc288bb0dac91d386dba
Oygul <oygulkhon3008@gmail.com>
2022-08-26 16:40:45 +0500

    responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let question = document.querySelectorAll('.faqs__ques');
-let answer = document.querySelector('#answer');
+let menuBtn = document.querySelector('#menuBtn');
+let closeBtn = document.querySelector('#closeBtn');
+let navBar = document.querySelector('#navBar');
+let navBg = document.querySelector('#navBg');
 
 question.forEach(item => {
     item.addEventListener("click", () => {
@@ -7,4 +10,15 @@ question.forEach(item => {
         // item.children.classList.add("bx-minus")
     })
    
+})
+
+menuBtn.addEventListener('click', ()=>{
+    navBar.classList.add('open-menu')
+    navBg.classList.add('open-bg');
+    closeBtn.classList.add('hide-btn')
+})
+closeBtn.addEventListener('click', ()=>{
+    navBar.classList.remove('open-menu')
+    navBg.classList.remove('open-bg')
+    closeBtn.classList.remove('hide-btn')
 })
