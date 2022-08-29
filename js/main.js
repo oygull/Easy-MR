80adf6c5ac1330f557217b09f9f7a69d462c8e15
Oygul <oygulkhon3008@gmail.com>
2022-08-29 14:55:20 +0500

    changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let menuBtn = document.querySelector('#menuBtn');
 let closeBtn = document.querySelector('#closeBtn');
 let navBar = document.querySelector('#navBar');
 let navBg = document.querySelector('#navBg');
+let changeLang = document.querySelector('#changeLang');
+let lang__item = document.querySelectorAll('.lang__item');
 
 question.forEach(item => {
     item.addEventListener("click", () => {
@@ -21,4 +23,15 @@ closeBtn.addEventListener('click', ()=>{
     navBar.classList.remove('open-menu')
     navBg.classList.remove('open-bg')
     closeBtn.classList.remove('hide-btn')
+})
+
+changeLang.addEventListener('click', ()=>{
+  document.querySelector('#langList').classList.toggle('hideList')
+})
+
+lang__item.forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelector('#langList').classList.remove('hideList')
+    })
+   
 })
